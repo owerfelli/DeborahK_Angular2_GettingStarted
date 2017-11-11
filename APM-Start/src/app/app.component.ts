c5dbdd26622ae097509c61861ebbcf6b63f92b34
owerfelli <o.werfelli@gmail.com>
2017-11-11 17:18:12 +0100

    Services and DI

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductService } from "./products/product.service";
 
 @Component({
   selector: 'pm-root',
   template: `<div><h1>{{pageTitle}}</h1>
     <pm-product></pm-product>
     </div>
-  `
+  `,
+  providers: [ProductService]
 })
 export class AppComponent{
   pageTitle: string ="Assalemou Alaykom";
