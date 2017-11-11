cb1d6828e944173eba1237942e6b1adf708bbc42
owerfelli <o.werfelli@gmail.com>
2017-11-11 16:04:52 +0100

    Filtred list

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div><h1>{{pageTitle}}</h1>
+    <pm-product></pm-product>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent{
+  pageTitle: string ="Assalemou Alaykom";
 }
---
+++ APM-Start/src/app/shared/convert-to-space.pipe.ts
@@ -0,0 +1,9 @@
+import {Pipe, PipeTransform} from '@angular/core'
+@Pipe({
+    name :'convertToSpace'
+})
+export class ConvertToSpacePipe implements PipeTransform {
+    transform(value: string, character:string):string{
+        return value.replace(character,' ');
+    }
+}
