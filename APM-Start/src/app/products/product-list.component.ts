e89648cb2156fd0762ceb868b174261a5a750638
owerfelli <o.werfelli@gmail.com>
2017-11-13 22:28:19 +0100

    Http request - Observable

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,26 +12,26 @@ export class ProductListComponent implements OnInit {
     imgWidth: number = 50;
     imgMargin: number = 2;
     showImages: boolean = false;
+    errorMessage: string;
 
-    
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
     }
     set listFilter(value: string) {
         this._listFilter = value;
-        this.filtredProducts = this.listFilter? this.performFilter(this.listFilter):this.products;
+        this.filtredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filtredProducts: IProduct[];
     products: IProduct[] = [];
 
-    constructor(private _productService: ProductService){
+    constructor(private _productService: ProductService) {
     }
 
-    performFilter(filterBy:string): IProduct[]{
+    performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
-            product.productName.toLocaleLowerCase().indexOf(filterBy) !==-1);
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
     toogleImage(): void {
@@ -39,11 +39,15 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products= this._productService.getProducts();
-        this.filtredProducts = this.products;
+        this._productService.getProducts()
+            .subscribe(products => {
+                this.products = products;
+                this.filtredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error);
     }
 
-    onRatingClicked(message:string):void{
+    onRatingClicked(message: string): void {
         console.log(message)
     }
 }
